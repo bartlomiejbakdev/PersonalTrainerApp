88f79d325bc317ad4b89561361ac63e3c2a3915a
bartlomiej-bak <barbak43@wp.pl>
2023-04-04 14:51:13 +0200

    Change font in templates, add new answer type to html

--- src/main/java/com/example/personaltrainer/Answer.java
+++ src/main/java/com/example/personaltrainer/Answer.java
@@ -11,5 +11,6 @@ import lombok.NoArgsConstructor;
 public class Answer {
 
     public String text;
+    public String label;
     public AnswerType type;
 }
--- src/main/java/com/example/personaltrainer/QuestionnaireService.java
+++ src/main/java/com/example/personaltrainer/QuestionnaireService.java
@@ -17,17 +17,17 @@ public class QuestionnaireService {
 
         List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("Jak się nazywasz?", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Twój E-mail", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Napisz mi o celach, które Cię interesują", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Twoja aktywność fizyczna", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Jakim planem treningowym obecnie trenowałeś?", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Masz jakieś kontuzje bądź odczuwasz dyskomfort przy ćwiczeniach?", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Ile jednostek treningowych w skali tygodnia jesteś w stanie wykonać?", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Podaj mi swoją wagę(na czczo) oraz wzrost.", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Pomiary", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Dieta", List.of(new Answer("", AnswerType.TEXT))));
-        questions.add(new Question("Dieta", List.of(new Answer("", AnswerType.TEXT))));
+        questions.add(new Question("Jak się nazywasz?", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Twój E-mail", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Napisz mi o celach, które Cię interesują", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Twoja aktywność fizyczna", List.of(new Answer("", "Duża", AnswerType.CHECKBOX), new Answer("", "Średnia", AnswerType.CHECKBOX), new Answer("", "Mała", AnswerType.CHECKBOX))));
+        questions.add(new Question("Jakim planem treningowym obecnie trenowałeś?", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Masz jakieś kontuzje bądź odczuwasz dyskomfort przy ćwiczeniach?", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Ile jednostek treningowych w skali tygodnia jesteś w stanie wykonać?", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Podaj mi swoją wagę(na czczo) oraz wzrost.", List.of(new Answer("", "Waga:", AnswerType.SMALLTEXT), new Answer("", "Wzrost:", AnswerType.SMALLTEXT))));
+        questions.add(new Question("Pomiary", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Dieta", List.of(new Answer("", "", AnswerType.TEXT))));
+        questions.add(new Question("Dieta", List.of(new Answer("", "", AnswerType.TEXT))));
 
         return new QuestionsDto(questions);
     }
