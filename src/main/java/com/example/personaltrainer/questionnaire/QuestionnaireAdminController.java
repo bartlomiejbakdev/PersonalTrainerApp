4fe78701c2927b29b6001bd105a2002c4c7fdcb1
Bartłomiej Bąk <barbak43@wp.pl>
2023-05-11 23:53:54 +0200

    Add page to edit Questionnaire on admin account

--- src/main/java/com/example/personaltrainer/questionnaire/QuestionnaireAdminController.java
+++ src/main/java/com/example/personaltrainer/questionnaire/QuestionnaireAdminController.java
@@ -1,6 +1,7 @@
 package com.example.personaltrainer.questionnaire;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class QuestionnaireAdminController {
 
+    private final QuestionnaireService questionnaireService;
+
+    public QuestionnaireAdminController(QuestionnaireService questionnaireService) {
+        this.questionnaireService = questionnaireService;
+    }
+
     @GetMapping
     public String adminHello() {
         return "admin";
+    }
+
+    @GetMapping("/kwestionariusz/edit")
+    public String editQuestionnaire(Model model) {
+        model.addAttribute("questions", questionnaireService.getQuestions());
+        return "admin-questionnaire-edit";
     }
 }
